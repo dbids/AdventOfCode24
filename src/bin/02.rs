4378c27def96807b6c827aec114f61a1a2942193
Devin Bidstrup <dbids@bu.edu>
2024-12-02 16:02:05 -0700

    Day 02 part 02 attempt 2
    Trying to be too smart

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -45,63 +45,74 @@ pub fn part_two(input: &str) -> Option<u32> {
       .map(|d| d.parse::<i64>().unwrap()) // Maps iter of strings to iter of u32
       .collect::<Vec<i64>>(); //Collects all elements of iter into vector
 
-    // Setup initial check for level
-    let mut one_unsafe: bool = false;
-    let mut change_direction: i32 = 0;
-    assert!(levels.len() > 1);
+    // Find first broken index in line (if any)
+    let mut is_safe: bool;
+    let broken_left_idx: usize;
+    (is_safe, broken_left_idx) = check_levels(&levels);
 
-    // Check if line is safe
-    for l_idx in 0..levels.len()-1 {
-      let diff = levels[l_idx] - levels[l_idx+1];
-
-      // Special case first diff
-      if l_idx == 0 {
-        change_direction = match levels[l_idx].cmp(&levels[l_idx+1]) {
-          Ordering::Less => -1,
-          Ordering::Greater => 1,
-          Ordering::Equal => {
-            one_unsafe = true;
-            0
-          },
-        };
-        if ((diff.abs() > 3) || (diff.abs() < 1)) && (one_unsafe == false) {
-          one_unsafe = true;
-        }
-      } else {
-
-        // Special case second diff with zero first diff
-        if (l_idx == 1) && (change_direction == 0) {
-          change_direction = match levels[l_idx].cmp(&levels[l_idx+1]) {
-            Ordering::Less => -1,
-            Ordering::Greater => 1,
-            Ordering::Equal => {
-              one_unsafe = true;
-              0
-            },
-          };
-          if change_direction == 0 {
-            continue 'report; // Unsafe
-          }
-        }
-
-        // Do generalized check for every level
-        if (diff == 0) || ((change_direction < 0) != (diff < 0)) ||
-          (diff.abs() > 3) || (diff.abs() < 1) {
-          if one_unsafe {
-            continue 'report;
-          } else {
-            one_unsafe = true;
-          }
-        }
-      }
-
-      println!("report: {report}\tl_idx: {l_idx}\tone_unsafe: {one_unsafe}");
+    // If no broken lines, increment and break
+    if is_safe {
+      println!("Safe Initially\t\tlevels:{:?}", levels.clone());
+      num_safe += 1;
+      continue 'report;
     }
 
-    // If we made it through the whole level it is safe
-    num_safe += 1;
+    // Try to fix a single broken level
+
+    // Remove left of problem
+    let mut levels_remove_left = levels.clone();
+    levels_remove_left.remove(broken_left_idx);
+    (is_safe, _ ) = check_levels(&levels_remove_left);
+    // println!("\t...\tlevels_remove_left:{:?}", levels_remove_left.clone());
+    if is_safe {
+      println!("Safe Remove Left\tlevels:{:?}\t\tlevels_remove_left:{:?}", levels.clone(), levels_remove_left);
+      num_safe += 1;
+      continue 'report;
+    }
+
+    if broken_left_idx > 0 {
+      // Remove right of problem
+      let mut levels_remove_right = levels.clone();
+      levels_remove_right.remove(broken_left_idx+1);
+      (is_safe, _ ) = check_levels(&levels_remove_right);
+      if is_safe {
+        println!("Safe Remove Right\tlevels:{:?}\t\tlevels_remove_right:{:?}", levels.clone(), levels_remove_right);
+        num_safe += 1;
+        continue 'report;
+      }
+
+      // Remove first element in case that was the problem
+      let mut levels_remove_first = levels.clone();
+      levels_remove_first.remove(0);
+      (is_safe, _ ) = check_levels(&levels_remove_first);
+      if is_safe {
+        println!("Safe Remove First\tlevels:{:?}\t\tlevels_remove_first:{:?}", levels.clone(), levels_remove_first);
+        num_safe += 1;
+        continue 'report;
+      }
+
+    }
+
+    println!("Not Safe\t\tlevels:{:?}", levels);
   }
   Some(num_safe)
+}
+
+// Check if line is safe
+fn check_levels(levels: &Vec<i64>) -> (bool, usize) {
+  let is_decreasing: bool = match levels[0].cmp(&levels[1]) {
+    Ordering::Less => true,
+    Ordering::Greater => false,
+    Ordering::Equal => return (false, 0),
+  };
+  for l_idx in 0..levels.len()-1 {
+    let diff = levels[l_idx] - levels[l_idx+1];
+    if (diff == 0) || (is_decreasing != (diff < 0)) ||
+      (diff.abs() > 3) || (diff.abs() < 1) {
+      return (false, l_idx);
+    }
+  }
+  return (true, 0);
 }
 
 #[cfg(test)]
