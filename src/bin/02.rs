73dad553fd3bc1ef84e12c4f29c2e063075187cf
Devin Bidstrup <dbids@bu.edu>
2024-12-02 16:11:39 -0700

    Day 2 part 02 solution

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -47,8 +47,7 @@ pub fn part_two(input: &str) -> Option<u32> {
 
     // Find first broken index in line (if any)
     let mut is_safe: bool;
-    let broken_left_idx: usize;
-    (is_safe, broken_left_idx) = check_levels(&levels);
+    is_safe = check_levels(&levels);
 
     // If no broken lines, increment and break
     if is_safe {
@@ -58,39 +57,16 @@ pub fn part_two(input: &str) -> Option<u32> {
     }
 
     // Try to fix a single broken level
-
-    // Remove left of problem
-    let mut levels_remove_left = levels.clone();
-    levels_remove_left.remove(broken_left_idx);
-    (is_safe, _ ) = check_levels(&levels_remove_left);
-    // println!("\t...\tlevels_remove_left:{:?}", levels_remove_left.clone());
-    if is_safe {
-      println!("Safe Remove Left\tlevels:{:?}\t\tlevels_remove_left:{:?}", levels.clone(), levels_remove_left);
-      num_safe += 1;
-      continue 'report;
-    }
-
-    if broken_left_idx > 0 {
-      // Remove right of problem
-      let mut levels_remove_right = levels.clone();
-      levels_remove_right.remove(broken_left_idx+1);
-      (is_safe, _ ) = check_levels(&levels_remove_right);
+    // Edit: we are doing this the brute force way
+    for l_idx in 0..levels.len() {
+      let mut levels_remove_idx = levels.clone();
+      levels_remove_idx.remove(l_idx);
+      is_safe = check_levels(&levels_remove_idx);
       if is_safe {
-        println!("Safe Remove Right\tlevels:{:?}\t\tlevels_remove_right:{:?}", levels.clone(), levels_remove_right);
+        println!("Safe Remove Idx\tlevels:{:?}\t\tlevels_remove_idx:{:?}", levels.clone(), levels_remove_idx);
         num_safe += 1;
         continue 'report;
       }
-
-      // Remove first element in case that was the problem
-      let mut levels_remove_first = levels.clone();
-      levels_remove_first.remove(0);
-      (is_safe, _ ) = check_levels(&levels_remove_first);
-      if is_safe {
-        println!("Safe Remove First\tlevels:{:?}\t\tlevels_remove_first:{:?}", levels.clone(), levels_remove_first);
-        num_safe += 1;
-        continue 'report;
-      }
-
     }
 
     println!("Not Safe\t\tlevels:{:?}", levels);
@@ -99,20 +75,20 @@ pub fn part_two(input: &str) -> Option<u32> {
 }
 
 // Check if line is safe
-fn check_levels(levels: &Vec<i64>) -> (bool, usize) {
+fn check_levels(levels: &Vec<i64>) -> bool {
   let is_decreasing: bool = match levels[0].cmp(&levels[1]) {
     Ordering::Less => true,
     Ordering::Greater => false,
-    Ordering::Equal => return (false, 0),
+    Ordering::Equal => return false,
   };
   for l_idx in 0..levels.len()-1 {
     let diff = levels[l_idx] - levels[l_idx+1];
     if (diff == 0) || (is_decreasing != (diff < 0)) ||
       (diff.abs() > 3) || (diff.abs() < 1) {
-      return (false, l_idx);
+      return false;
     }
   }
-  return (true, 0);
+  return true;
 }
 
 #[cfg(test)]
