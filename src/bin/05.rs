fb8907068bdfceba38aa4d66ae2e644b55cf48ca
Devin Bidstrup <dbids@bu.edu>
2024-12-05 22:40:17 -0700

    Day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -22,7 +22,7 @@ pub fn part_one(input: &str) -> Option<u64> {
     rules[bef].push(aft);
   }
 
-  // Check updates for validity
+  // Check updates for validity and sum valid ones
   let mut mid_sum: u64 = 0;
   'updates: for update in updates_str.split_whitespace() {
     // Get string as a vector
@@ -46,8 +46,60 @@ pub fn part_one(input: &str) -> Option<u64> {
   return Some(mid_sum);
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-  None
+pub fn part_two(input: &str) -> Option<u64> {
+  // Split input into two at blank line
+  let Some(split_idx) = input.find("\n\n") else {
+    return None;
+  };
+  let (rules_str, updates_str) = input.split_at(split_idx);
+  let updates_str = &updates_str[2..]; // Cutoff \n\n
+
+  // Create array of vectors to hold rules
+  const NEW_VEC: Vec<u8> = Vec::new();
+  let mut rules = [NEW_VEC; 100];
+  let rules_re = Regex::new(r"([0-9]+)\|([0-9]+)").unwrap(); // search for mul
+  for (_, [bef, aft]) in rules_re.captures_iter(rules_str).map(|c| c.extract()) {
+    let bef = bef.parse::<usize>().unwrap();
+    let aft = aft.parse::<u8>().unwrap();
+    rules[bef].push(aft);
+  }
+
+  // Check updates for validity and sum invalid ones after reordering
+  let mut mid_sum: u64 = 0;
+  for update in updates_str.split_whitespace() {
+    // Get string as a vector
+    let mut update_vec : Vec<u8> = update
+      .split(',')
+      .map(|d| d.parse::<u8>().unwrap()) // Maps iter of strings to iter of u8
+      .collect::<Vec<u8>>(); //Collects all elements of iter into vector
+
+    // iterate over vector, checking rules at each index
+    let mut incorrect_at_first = false;
+    'fixed: loop {
+      let mut valid_update = true;
+      for uv_idx in 0..update_vec.len() {
+        let r_idx: usize = update_vec[uv_idx] as usize;
+        for rule in rules[r_idx].clone().into_iter() {
+          if let Some(m_idx) = &update_vec[0..uv_idx].iter().position(|&d| d==rule) {
+            // This rule is broken, so fix it
+            incorrect_at_first = true;
+            valid_update = false;
+            let rule_breaker = update_vec.remove(*m_idx);
+            update_vec.insert(uv_idx, rule_breaker);
+          }
+        }
+      }
+      // Break out for already correct rules
+      if valid_update {
+        break 'fixed;
+      }
+    }
+
+    if incorrect_at_first {
+      mid_sum += update_vec[update_vec.len()/2] as u64;
+    }
+  }
+  return Some(mid_sum);
 }
 
 #[cfg(test)]
@@ -63,6 +115,6 @@ mod tests {
   #[test]
   fn test_part_two() {
     let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-    assert_eq!(result, None);
+    assert_eq!(result, Some(123));
   }
 }
