4794f7ab73effeba0e9b4f42bd9a369fb93e23cd
dbids <dbidstrup18@gmail.com>
2024-12-13 21:01:34 -0700

    Day 09 part 1

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,31 +1,108 @@
 advent_of_code::solution!(9);
 
 // ##################### DEPENDENCIES ####################
+use std::cmp::Ordering;
+use std::collections::VecDeque;
 
 // ##################### TYPE DEFS #####################
 #[derive(Debug)]
 struct Metadata {
   id_num: usize,
   start: usize,
-  end: usize,
+  len: usize,
 }
 // ##################### HELPER FUNCTIONS ####################
 
 // ##################### PART ONE #####################
-pub fn part_one(input: &str) -> Option<u32> {
-  let mut files: Vec<Metadata> = Vec::new();
-  let mut spaces: Vec<Metadata> = Vec::new();
+pub fn part_one(input: &str) -> Option<usize> {
+
+  // Get input as metadata
+  let mut files: VecDeque<Metadata> = VecDeque::new();
+  let mut spaces: VecDeque<Metadata> = VecDeque::new();
   let mut counter: usize = 0;
-  input.chars().map(|c| c.to_digit(10).unwrap()).enumerate().for_each(|(c_idx, c)| {
-    if c_idx % 2 {
-      files.push(Metadata{id_num: c_idx/2, start:counter, end:counter+c-1});
-    } else {
-      spaces.push(Metadata{id_num: 0, start:counter, end:counter+c-1});
+
+  input
+    .chars()
+    .filter(|c| c.is_numeric())
+    .map(|c| c.to_digit(10).unwrap() as usize)
+    .enumerate()
+    .for_each(|(c_idx, c)| {
+      if c != 0 {
+        if (c_idx % 2) == 0 {
+          files.push_back(Metadata {
+            id_num: c_idx / 2,
+            start: counter,
+            len: c,
+          });
+        } else {
+          spaces.push_back(Metadata {
+            id_num: 0,
+            start: counter,
+            len: c,
+          });
+        }
+        counter += c;
+      }
+    });
+
+  //dbg!(&files, &spaces);
+
+  // Compress data
+  let Some(mut curr_file) = files.pop_back() else {
+    panic!("No files")
+  };
+  let Some(mut curr_space)= spaces.pop_front() else {
+    panic!("No spaces")
+  };
+  'Compress: loop {
+    if curr_file.start < curr_space.start {
+      files.push_back(curr_file);
+      break 'Compress;
     }
-    counter += c;
-  })
+    match curr_space.len.cmp(&curr_file.len) {
+      Ordering::Greater => {
+        //dbg!("Greater", &curr_space, &curr_file);
+        files.push_front(Metadata{start:curr_space.start, ..curr_file});
+        curr_space.start += curr_file.len;
+        curr_space.len -= curr_file.len;
+        curr_file = files.pop_back().unwrap_or_else(|| panic!("Ran out of files"));
+      }
+      Ordering::Equal => {
+        //dbg!("Equal", &curr_space, &curr_file);
+        files.push_front(Metadata{start:curr_space.start, ..curr_file});
+        curr_file = files.pop_back().unwrap_or_else(|| panic!("Ran out of files"));
+        if let Some(some_space) = spaces.pop_front() {
+          curr_space = some_space;
+        } else {
+          break 'Compress;
+        };
+      }
+      Ordering::Less => {
+        //dbg!("Less", &curr_space, &curr_file);
+        files.push_front(Metadata{start:curr_space.start, id_num:curr_file.id_num, len:curr_space.len});
+        curr_file.len -= curr_space.len;
+        if let Some(some_space) = spaces.pop_front() {
+          curr_space = some_space;
+        } else {
+          break 'Compress;
+        };
+      }
+    }
+  }
 
+  //let mut sorted_files = Vec::from(files);
+  //sorted_files.sort_by(|v1, v2| v1.start.cmp(&v2.start));
+  //dbg!(sorted_files);
 
+  // Calculate checksum
+  let mut checksum: usize = 0;
+  for f in files.into_iter() {
+    for offst in 0..f.len {
+      checksum += f.id_num * (f.start + offst);
+    }
+  }
+
+  Some(checksum)
 }
 
 // ##################### PART TWO ####################
