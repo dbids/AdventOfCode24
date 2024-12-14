08e91cc811402acbd138812cd4bfc5c71d5e670a
dbids <dbidstrup18@gmail.com>
2024-12-13 21:07:03 -0700

    Cleanup Day 9 part 1

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -11,16 +11,10 @@ struct Metadata {
   start: usize,
   len: usize,
 }
+
 // ##################### HELPER FUNCTIONS ####################
-
-// ##################### PART ONE #####################
-pub fn part_one(input: &str) -> Option<usize> {
-
-  // Get input as metadata
-  let mut files: VecDeque<Metadata> = VecDeque::new();
-  let mut spaces: VecDeque<Metadata> = VecDeque::new();
+fn format_input(input: &str, files: &mut VecDeque<Metadata>, spaces: &mut VecDeque<Metadata>) {
   let mut counter: usize = 0;
-
   input
     .chars()
     .filter(|c| c.is_numeric())
@@ -44,14 +38,20 @@ pub fn part_one(input: &str) -> Option<usize> {
         counter += c;
       }
     });
+}
 
-  //dbg!(&files, &spaces);
+// ##################### PART ONE #####################
+pub fn part_one(input: &str) -> Option<usize> {
+  // Get input as metadata
+  let mut files: VecDeque<Metadata> = VecDeque::new();
+  let mut spaces: VecDeque<Metadata> = VecDeque::new();
+  format_input(&input, &mut files, &mut spaces);
 
   // Compress data
   let Some(mut curr_file) = files.pop_back() else {
     panic!("No files")
   };
-  let Some(mut curr_space)= spaces.pop_front() else {
+  let Some(mut curr_space) = spaces.pop_front() else {
     panic!("No spaces")
   };
   'Compress: loop {
@@ -61,16 +61,24 @@ pub fn part_one(input: &str) -> Option<usize> {
     }
     match curr_space.len.cmp(&curr_file.len) {
       Ordering::Greater => {
-        //dbg!("Greater", &curr_space, &curr_file);
-        files.push_front(Metadata{start:curr_space.start, ..curr_file});
+        files.push_front(Metadata {
+          start: curr_space.start,
+          ..curr_file
+        });
         curr_space.start += curr_file.len;
         curr_space.len -= curr_file.len;
-        curr_file = files.pop_back().unwrap_or_else(|| panic!("Ran out of files"));
+        curr_file = files
+          .pop_back()
+          .unwrap_or_else(|| panic!("Ran out of files"));
       }
       Ordering::Equal => {
-        //dbg!("Equal", &curr_space, &curr_file);
-        files.push_front(Metadata{start:curr_space.start, ..curr_file});
-        curr_file = files.pop_back().unwrap_or_else(|| panic!("Ran out of files"));
+        files.push_front(Metadata {
+          start: curr_space.start,
+          ..curr_file
+        });
+        curr_file = files
+          .pop_back()
+          .unwrap_or_else(|| panic!("Ran out of files"));
         if let Some(some_space) = spaces.pop_front() {
           curr_space = some_space;
         } else {
@@ -78,8 +86,11 @@ pub fn part_one(input: &str) -> Option<usize> {
         };
       }
       Ordering::Less => {
-        //dbg!("Less", &curr_space, &curr_file);
-        files.push_front(Metadata{start:curr_space.start, id_num:curr_file.id_num, len:curr_space.len});
+        files.push_front(Metadata {
+          start: curr_space.start,
+          id_num: curr_file.id_num,
+          len: curr_space.len,
+        });
         curr_file.len -= curr_space.len;
         if let Some(some_space) = spaces.pop_front() {
           curr_space = some_space;
@@ -89,10 +100,6 @@ pub fn part_one(input: &str) -> Option<usize> {
       }
     }
   }
-
-  //let mut sorted_files = Vec::from(files);
-  //sorted_files.sort_by(|v1, v2| v1.start.cmp(&v2.start));
-  //dbg!(sorted_files);
 
   // Calculate checksum
   let mut checksum: usize = 0;
