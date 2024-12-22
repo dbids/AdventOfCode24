cbc4fe8a5edae7b169ff57753d91f476f46450d2
Devin Bidstrup <dbids@bu.edu>
2024-12-22 13:08:32 -0700

    Setup Day 10-12

---
+++ src/bin/10.rs
@@ -0,0 +1,35 @@
+advent_of_code::solution!(10);
+
+// ##################### DEPENDENCIES ####################
+
+// ##################### TYPE DEFS #####################
+
+// ##################### HELPER FUNCTIONS ####################
+
+// ##################### PART ONE #####################
+pub fn part_one(input: &str) -> Option<u32> {
+  None
+}
+
+// ##################### PART TWO ####################
+pub fn part_two(input: &str) -> Option<u32> {
+  None
+}
+
+// ##################### TESTS ###################
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_part_one() {
+    let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+    assert_eq!(result, None);
+  }
+
+  #[test]
+  fn test_part_two() {
+    let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+    assert_eq!(result, None);
+  }
+}
